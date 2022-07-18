a0def1e12d4617bca81d8e2d8a9283d8676b018d
deepak sharma <deepak.sharma@moglix.com>
2022-07-18 16:51:39 +0530

    save data in mysql with sequelize with modular way

---
+++ edu/sequlize-app/config/database.js
@@ -0,0 +1,8 @@
+import { Sequelize } from "sequelize";
+ const db = new Sequelize('sequelize_db', 'root', '', {
+    host: 'localhost',
+    user:'root',
+    password:'Password123@',
+    dialect: 'mysql'
+});
+export default db;
